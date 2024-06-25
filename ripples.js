573ac43ece751e9d713d955689c4cb6b1db33b82
Mkkas3145 <76113903+Mkkas3145@users.noreply.github.com>
2024-06-26 04:27:23 +0900

    Update ripples.js

--- ripples.js
+++ ripples.js
@@ -48,6 +48,7 @@ class RipplesEvent {
         ripples.style.transition = ("opacity " + this.option["fadeInDuration"] + "s");
         ripples.style.opacity = 0;
         let effect = document.createElement("div");
+        effect.classList.add("ripples_element_effect");
         effect.style.backgroundColor = this.option["color"];
         effect.style.borderRadius = "50%";
         effect.style.opacity = this.option["opacity"];
@@ -112,6 +113,114 @@ class RipplesEvent {
     }
 }
 
+class LightHoverEvent {
+    static option = {
+        "enable": true,             // 호버 효과 사용 여부
+        "fadeInDuration": 0.2,      // 호버 효과가 나타나는 시간
+        "fadeOutDuration": 0.2,     // 호버 효과가 끝난 뒤, 사라지는 시간
+        "color": "#000000",         // 호버 효과 색상
+        "opacity": 0.5,             // 호버 효과 불투명도
+        "scale": 1                  // 호버 효과 크기
+    }
+
+    static start(element, x, y) {
+        if (this.option["enable"] == false) { return; }
+        let rect = element.getBoundingClientRect();
+        
+        // 절대 위치를 상대 위치로 계산합니다.
+        x -= rect.left;
+        y -= rect.top;
+
+        // 마우스 지점을 퍼센트로 계산합니다.
+        let percentX = (x / rect.width);
+        let percentY = (y / rect.height);
+        // 퍼센트(0 ~ 1)로 계산된 것을 -0.5 ~ 0.5 범위로 바꿉니다.
+        percentX -= 0.5;
+        percentY -= 0.5;
+        // -0.5 ~ 0.5 범위로 계산된 것을 -1 ~ 1 범위로 바꿉니다.
+        percentX *= 2;
+        percentY *= 2;
+
+        let wrap = element.getElementsByClassName("light_hover_element_wrap");
+        if (wrap.length == 0) {
+            // 호버 효과 앨리먼트 생성
+            let lightHover = document.createElement("div");
+            lightHover.classList.add("light_hover_element_wrap");
+            lightHover.style.width = "100%";
+            lightHover.style.height = "100%";
+            lightHover.style.position = "absolute";
+            lightHover.style.top = "0px";
+            lightHover.style.left = "0px";
+            lightHover.style.zIndex = 2147483647;
+            lightHover.style.pointerEvents = "none";
+            lightHover.style.display = "flex";
+            lightHover.style.justifyContent = "center";
+            lightHover.style.alignItems = "center";
+            lightHover.style.overflow = "hidden";
+            lightHover.style.transition = ("opacity " + this.option["fadeInDuration"] + "s");
+            lightHover.style.opacity = 0;
+            let effect = document.createElement("div");
+            effect.classList.add("light_hover_element_effect");
+            effect.style.backgroundColor = this.option["color"];
+            effect.style.maskImage = "radial-gradient(circle, white 0%, transparent 70%)";
+            effect.style.borderRadius = "50%";
+            effect.style.opacity = this.option["opacity"];
+            effect.style.transform = ("scale(" + this.option["scale"] + ")");
+            
+            // 최대 사이즈 구하기 (거리 차이를 이용해서)
+            let centerToWidth = Math.abs(rect.width * percentX);
+            let centerToHeight = Math.abs(rect.height * percentY);
+            let toWidth = (rect.width + centerToWidth);
+            let toHeight = (rect.height + centerToHeight);
+            let size = this.distanceTo(0, 0, toWidth, toHeight);
+            effect.style.minWidth = (size + "px");
+            effect.style.minHeight = (size + "px");
+            // 호버 효과를 마우스 지점으로 이동
+            effect.style.marginTop = ((rect.height * percentY) + "px");
+            effect.style.marginLeft = ((rect.width * percentX) + "px");
+
+            lightHover.appendChild(effect);
+            element.appendChild(lightHover);
+
+            // 호버 효과 나타나기
+            window.requestAnimationFrame(() => {
+                window.requestAnimationFrame(() => {
+                    lightHover.style.opacity = 1;
+                });
+            });
+        } else {
+            let effect = wrap[0].children[0];
+            // 최대 사이즈 구하기 (거리 차이를 이용해서)
+            let centerToWidth = Math.abs(rect.width * percentX);
+            let centerToHeight = Math.abs(rect.height * percentY);
+            let toWidth = (rect.width + centerToWidth);
+            let toHeight = (rect.height + centerToHeight);
+            let size = this.distanceTo(0, 0, toWidth, toHeight);
+            effect.style.minWidth = (size + "px");
+            effect.style.minHeight = (size + "px");
+            // 호버 효과를 터치한 지점으로 이동
+            effect.style.marginTop = ((rect.height * percentY) + "px");
+            effect.style.marginLeft = ((rect.width * percentX) + "px");
+            wrap[0].style.transition = ("opacity " + this.option["fadeOutDuration"] + "s");
+            wrap[0].style.opacity = 1;
+        }
+    }
+
+    static end(element) {
+        let wrap = element.getElementsByClassName("light_hover_element_wrap");
+        if (wrap.length != 0) {
+            wrap[0].style.opacity = 0;
+        }
+    }
+    
+    // 거리 차이 구하기
+    static distanceTo(x1, y1, x2, y2) {
+        let xDistance = (x2 - x1);
+        let yDistance = (y2 - y1);
+        return Math.sqrt(Math.pow(xDistance, 2) + Math.pow(yDistance, 2));
+    }
+}
+
 class RipplesElement extends HTMLElement {
     constructor() {
         super();
@@ -124,6 +233,12 @@ class RipplesElement extends HTMLElement {
         }
         // 리플 효과 제거
         let wrap = this.getElementsByClassName("ripples_element_wrap");
+        for (let i = 0; i < wrap.length; i++) {
+            wrap[i].remove();
+            i --;
+        }
+        // 호버 효과 제거
+        wrap = this.getElementsByClassName("light_hover_element_wrap");
         for (let i = 0; i < wrap.length; i++) {
             wrap[i].remove();
             i --;
@@ -143,12 +258,25 @@ class RipplesElement extends HTMLElement {
         child.addEventListener("mouseleave", () => {
             RipplesEvent.end(child);
         });
+        // 이벤트 등록 - 마우스 이동
+        let isTouch_mousemove = false;
+        child.addEventListener("mousemove", (event) => {
+            if (!isTouch_mousemove) {
+                LightHoverEvent.start(child, event.clientX, event.clientY);
+            } else {
+                isTouch_mousemove = false;
+            }
+        });
+        child.addEventListener("mouseleave", () => {
+            LightHoverEvent.end(child);
+        });
         // 이벤트 등록 - 터치
         let isTouch = false;
         let touchTimeout = null;
         let touchStartEvent = null;
         child.addEventListener("touchstart", (event) => {
             isTouch = true;
+            isTouch_mousemove = true;
             touchStartEvent = event;
             touchStart(event, RipplesEvent.option["touchDelay"]);
         }, {passive: true});
